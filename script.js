49fde6dcb276df88b15de3f9e1b261c3206b532c
João Pedro Carvalho <j.pcarvalho1301@gmail.com>
2024-04-04 10:34:44 -0300

    teste

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function btn(){
     const form = document.querySelector("form");
+    
     const cadastro = {
          ativ: form.ativ.value,
          desc: form.desc.value,
          date: form.date.value
     }
-    validar(cadastro);
+    return validar(cadastro);
 }
 
 function validar(cadastro){
-    if(cadastro.ativ === ""){
-        alert("error")
+    if(cadastro.ativ === "" || cadastro.desc === "" || cadastro.date === "")){
+        const label = document.querySelector("label);
+        label.style.color = "red";                                   
+        alert("preencha todos os campos !")
     }
+     
 }
