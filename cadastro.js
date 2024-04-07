0783490708d209cd3a2b478c2f391b5847cf9599
João <j.pcarvalho1301@gmail.com>
2024-04-06 23:04:08 -0300

    localStorage

---
+++ cadastro.js
@@ -0,0 +1,43 @@
+let Array_Ativ = [];
+function btn() {
+    const form = document.querySelector("form");
+
+    const cadastro = {
+        ativ: form.ativ.value,
+        desc: form.desc.value,
+        date: form.date.value
+    }
+    return validar(cadastro);
+}
+
+function validar(cadastro) {
+    if (cadastro.ativ === "" || cadastro.desc === "" || cadastro.date === "") {
+        alert("preencha todos os campos !")
+    } else {
+        if (Array_Ativ.length == 0) {
+            console.log(Array_Ativ.length)
+            const cadastro_atividade = new atividade(cadastro.ativ, cadastro.desc, cadastro.date)
+            Array_Ativ.push(cadastro_atividade);
+            const Array_Json = JSON.stringify(Array_Ativ);
+            console.log(Array_Json);
+            localStorage.setItem("Atividade", Array_Json);
+        }
+    }
+}
+
+class atividade {
+    constructor(ativ, desc, date) {
+        this.ativ = ativ;
+        this.desc = desc;
+        this.date = date;
+    }
+    getAtiv() {
+        return this.ativ;
+    }
+    getDesc() {
+        return this.desc;
+    }
+    getDate() {
+        return this.date;
+    }
+}
--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function btn(){
-    const form = document.querySelector("form");
-    
-    const cadastro = {
-         ativ: form.ativ.value,
-         desc: form.desc.value,
-         date: form.date.value
-    }
-    return validar(cadastro);
-}
+const busca_localoStorage = localStorage.getItem("Atividade");
+const Array_criar = JSON.parse(busca_localoStorage);
+const section = document.querySelector("section");
 
-function validar(cadastro){
-    if(cadastro.ativ === "" || cadastro.desc === "" || cadastro.date === "")){
-        const label = document.querySelector("label);
-        label.style.color = "red";                                   
-        alert("preencha todos os campos !")
-    }
-     
-}
+Array_criar.forEach(atividade => {
+    const atividadeDiv = document.createElement("div");
+    atividadeDiv.classList.add("container-task", "nes-container", "is-dark", "with-title");
+    atividadeDiv.innerHTML = `
+        <p class="title">${atividade.ativ}</p>
+        <p class="Subtitle">${atividade.desc}</p>
+        <a href="#" class="nes-badge is-icon">
+            <span class="is-warning"><i class="nes-icon star is-small"></i></span>
+            <span class="is-primary">Icons</span>
+        </a>
+        <progress class="nes-progress is-success" value="80" max="100"></progress>
+        <div class="container_icons">
+            <button type="button" class="nes-btn is-warning">-</button>
+            <button type="button" class="nes-btn is-primary">+</button>
+            <button type="button" class="nes-btn is-error">Apagar</button>
+        </div>`;
+    section.appendChild(atividadeDiv);
+});
