d15610729fa8ed798e6781cea3cfbeb4863b9e02
João <j.pcarvalho1301@gmail.com>
2024-04-21 00:12:09 -0300

    novo projeto

--- script.js
+++ script.js
@@ -1,22 +1,96 @@
-const busca_localoStorage = localStorage.getItem("Atividade");
-const Array_criar = JSON.parse(busca_localoStorage);
-const section = document.querySelector("section");
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sDesc = document.querySelector('#m-desc')
+const sdata = document.querySelector('#m-data')
+const btnSalvar = document.querySelector('#btnSalvar')
 
-Array_criar.forEach(atividade => {
-    const atividadeDiv = document.createElement("div");
-    atividadeDiv.classList.add("container-task", "nes-container", "is-dark", "with-title");
-    atividadeDiv.innerHTML = `
-        <p class="title">${atividade.ativ}</p>
-        <p class="Subtitle">${atividade.desc}</p>
-        <a href="#" class="nes-badge is-icon">
-            <span class="is-warning"><i class="nes-icon star is-small"></i></span>
-            <span class="is-primary">Icons</span>
-        </a>
-        <progress class="nes-progress is-success" value="80" max="100"></progress>
-        <div class="container_icons">
-            <button type="button" class="nes-btn is-warning">-</button>
-            <button type="button" class="nes-btn is-primary">+</button>
-            <button type="button" class="nes-btn is-error">Apagar</button>
-        </div>`;
-    section.appendChild(atividadeDiv);
-});
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+  modal.classList.add('active')
+
+  modal.onclick = e => {
+    if (e.target.className.indexOf('modal-container') !== -1) {
+      modal.classList.remove('active')
+    }
+  }
+
+  if (edit) {
+    sNome.value = itens[index].nome
+    sDesc.value = itens[index].Descricao
+    sdata.value = itens[index].data
+    id = index
+  } else {
+    sNome.value = ''
+    sDesc.value = ''
+    sdata.value = ''
+  }
+  
+}
+
+function editItem(index) {
+
+  openModal(true, index)
+}
+
+function deleteItem(index) {
+  itens.splice(index, 1)
+  setItensBD()
+  loadItens()
+}
+
+function insertItem(item, index) {
+  let tr = document.createElement('tr')
+    tr.classList.add("container-task", "nes-container", "with-title","task");
+    tr.innerHTML = `
+    <td>${item.nome}</td>
+    <td>${item.Descricao}</td>
+    <td>${item.data}</td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+  tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+  
+  if (sNome.value == '' || sDesc.value == '' || sdata.value == '') {
+    return
+  }
+
+  e.preventDefault();
+
+  if (id !== undefined) {
+    itens[id].nome = sNome.value
+    itens[id].Descricao = sDesc.value
+    itens[id].data = sdata.value
+  } else {
+    itens.push({'nome': sNome.value, 'Descricao': sDesc.value, 'data': sdata.value})
+  }
+
+  setItensBD()
+
+  modal.classList.remove('active')
+  loadItens()
+  id = undefined
+}
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
